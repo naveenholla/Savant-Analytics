99600c4861984c3d1f609f294da86f6609d12896
Arnav Gudibande <arnav.gudibande@gmail.com>
2017-02-01 21:40:11 -0800

    Added Stocks module

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
-        Calendar from = Calendar.getInstance();
-        Calendar to = Calendar.getInstance();
-        from.add(Calendar.YEAR, -5); // from 5 years ago
+        Stock tesla = YahooFinance.get("TSLA", true);
+        System.out.println(tesla.getHistory());
 
-        //Stock google = YahooFinance.get("GOOG", from, to, Interval.WEEKLY);
-        Stock stock = YahooFinance.get("AAPL");
-        BigDecimal price = stock.getQuote().getPrice();
-        BigDecimal change = stock.getQuote().getChangeInPercent();
-        BigDecimal peg = stock.getStats().getPeg();
-        BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-
-        stock.print();
+        Stock apple = YahooFinance.get("AAPL");
+        BigDecimal price = apple.getQuote().getPrice();
+        BigDecimal change = apple.getQuote().getChangeInPercent();
+        BigDecimal peg = apple.getStats().getPeg();
+        BigDecimal dividend = apple.getDividend().getAnnualYieldPercent();
+        apple.print();
+        System.out.println(price);
     }
 }
